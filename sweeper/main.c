ebad1b0c6e49f15f277b617858a064acda275072
moogloof <ethansyun@icloud.com>
2023-12-04 23:51:59 -0800

    Made basic bouncing thing

--- sweeper/main.c
+++ sweeper/main.c
@@ -6,9 +6,34 @@ extern void init_framebuffer();
 extern void draw_rect(u32, u32, u32, u32, u32);
 
 void main() {
-	init_framebuffer();
-	draw_rect(400, 400, 200, 200, 0xff00ff);
+	u32 x = 0;
+	u32 y = 0;
+	u32 xvel = 1;
+	u32 yvel = 1;
+	u32 w = 100;
+	u32 h = 100;
+	u32 color = 0x0000ff;
 
-	// Halt
-	while (1) {}
+	init_framebuffer();
+
+	// Bouncing thingy
+	while (1) {
+		draw_rect(0, 0, 1920, 1080, 0);
+		x += xvel;
+		y += yvel;
+
+		if (x + w >= 1920) {
+			xvel = -1;
+		} else if (x <= 0) {
+			xvel = 1;
+		}
+
+		if (y + h >= 1080) {
+			yvel = -1;
+		} else if (y <= 0) {
+			yvel = 1;
+		}
+
+		draw_rect(x, y, w, h, color);
+	}
 }
