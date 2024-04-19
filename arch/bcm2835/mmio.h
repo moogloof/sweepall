6f088822349f8e08617955afe06c167c4249be53
moogloof <ethansyun@icloud.com>
2024-04-18 18:04:17 -0700

    Fixed memory map for bcm2835 and mcpu

--- arch/bcm2835/mmio.h
+++ arch/bcm2835/mmio.h
@@ -5,7 +5,7 @@
 // See BCM2835 ARM Peripherals 1.2.3
 // All port addresses will be written as peripheral (translated by the mmio_read/write functions)
 #define PERIPHERAL_BASE 0x7e000000
-#define PHYSICAL_BASE 0x3f000000
+#define PHYSICAL_BASE 0x20000000
 
 // Type stuff
 // NOTE: May change depending on build system
